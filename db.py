a443d2e3b30b16fb6ac70fb69f0de934242ed289
MiChat <ms9whgswzb@privaterelay.appleid.com>
2025-11-20 17:04:48 +0100

    Create db.py

---
+++ db.py
@@ -0,0 +1,18 @@
+# db.py
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./chat.db")
+
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
